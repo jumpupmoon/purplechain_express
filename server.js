eb4d4f241cbfbf4c6a8fcae6917079058ba2692d
unknown <jinjihoon@gmail.com>
2020-08-06 09:46:02 +0900

    [jin] 처방전 결과 라우터 수정

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const descriptionRouter = require("./routes/descriptionRouter");
 const descriptionMainRouter = require("./routes/descriptionMainRouter");
+const descriptions = require("./routes/descriptions");
 const patientRouter = require("./routes/patientRouter");
 const diseaseRouter = require("./routes/diseaseRouter");
 const medicineRouter = require("./routes/medicineRouter");
 
 app.use("/api/description", descriptionRouter);
 app.use("/api/description", descriptionMainRouter);
+app.use("/api/descriptions", descriptions);
 app.use("/api/board", boardRoute);
 app.use("/api/patient", patientRouter);
 //질병 정보와 관계된 라우터 등록
@@ -25,9 +28,6 @@ app.use("/api/disease", diseaseRouter);
 //의약품 정보와 관련된 라우터 등록
 app.use("/api/medicine", medicineRouter);
 
-app.use(cors());
-
-app.use("/api/descriptions", require("./routes/descriptions"));
 app.use("/api/doctors", require("./routes/doctors"));
 
 app.listen(port, function () {
